3736edc9438fa25a886a94582efe10f2d1657fdc
Denys Fedoryshchenko <denys.f@collabora.com>
2026-09-21 12:43:28 +0300

    deps: pin octseq to FastNetMon mirror
    Keep octseq 0.5.2 at its original release commit, sourced from FastNetMon. Refresh the fuzz lockfile and borrow its session configuration to match the current parser API.

--- fuzz/fuzz_targets/parse_bgp_message.rs
+++ fuzz/fuzz_targets/parse_bgp_message.rs
@@ -4,8 +4,7 @@ use libfuzzer_sys::fuzz_target;
 use routecore::bgp::message::{Message, SessionConfig};
 
 fuzz_target!(|data: (&[u8], Option<SessionConfig>)| {
-    let _ =  Message::from_octets(data.0, data.1);
+    let _ =  Message::from_octets(data.0, data.1.as_ref());
 });
-
 
 
